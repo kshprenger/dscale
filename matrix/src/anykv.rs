07f6740e7fe9cf138a027b57fa684735844045fb
kshprenger <kostya@shprenger.com>
2026-01-05 10:09:38 +0100

    Rename

--- matrix/src/anykv.rs
+++ matrix/src/anykv.rs
@@ -3,17 +3,17 @@ use std::cell::RefCell;
 use std::collections::HashMap;
 
 thread_local! {
-    pub(crate) static METRICS: RefCell<HashMap<String, Box<dyn Any>>> = RefCell::new(HashMap::new());
+    pub(crate) static ANY_KV: RefCell<HashMap<String, Box<dyn Any>>> = RefCell::new(HashMap::new());
 }
 
 pub fn Set<T: 'static>(key: &str, value: T) {
-    METRICS.with(|m| {
+    ANY_KV.with(|m| {
         m.borrow_mut().insert(key.to_string(), Box::new(value));
     });
 }
 
 pub fn Get<T: 'static + Clone>(key: &str) -> T {
-    METRICS.with(|m| {
+    ANY_KV.with(|m| {
         m.borrow()
             .get(key)
             .and_then(|v| v.downcast_ref::<T>())
@@ -23,7 +23,7 @@ pub fn Get<T: 'static + Clone>(key: &str) -> T {
 }
 
 pub fn Modify<T: 'static>(key: &str, f: impl FnOnce(&mut T)) {
-    METRICS.with(|m| {
+    ANY_KV.with(|m| {
         if let Some(value) = m.borrow_mut().get_mut(key) {
             if let Some(typed_value) = value.downcast_mut::<T>() {
                 f(typed_value);
@@ -33,7 +33,7 @@ pub fn Modify<T: 'static>(key: &str, f: impl FnOnce(&mut T)) {
 }
 
 pub fn Clear() {
-    METRICS.with(|m| {
+    ANY_KV.with(|m| {
         m.borrow_mut().clear();
     });
 }
